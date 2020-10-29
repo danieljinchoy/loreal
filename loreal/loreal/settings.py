9b7aab89ecd0ed256d65df19a200315eb8c56597
Daniel Choy <danieljinchoy@gmail.com>
2020-10-29 17:59:48 -0400

    some updates

--- loreal/loreal/settings.py
+++ loreal/loreal/settings.py
@@ -17,7 +17,7 @@ NEWSPIDER_MODULE = 'loreal.spiders'
 #USER_AGENT = 'loreal (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
@@ -25,7 +25,7 @@ ROBOTSTXT_OBEY = True
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-#DOWNLOAD_DELAY = 3
+DOWNLOAD_DELAY = 2
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
@@ -62,8 +62,8 @@ ROBOTSTXT_OBEY = True
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    'loreal.pipelines.LorealPipeline': 300,
+ITEM_PIPELINES = {
+    'loreal.pipelines.LorealPipeline': 300,
 #}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
---
+++ loreal/loreal/spiders/loreal_spider.py
@@ -0,0 +1,12 @@
+from scrapy import Spider, Request
+from loreal.items import LorealItem
+
+class LorealSpider(Spider):
+	name = 'loreal_spider'
+
+	start_urls = 'https://www.lorealparisusa.com/products/hair-care/shop-all-products.aspx?size=21&page=1'
+
+	allowed_urls = 'https://www.lorealparisusa.com/'
+
+	
+
