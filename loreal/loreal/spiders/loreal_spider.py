fb35a12d01192ac6e8eead47a93e7413a4e38010
Daniel Choy <danieljinchoy@gmail.com>
2020-10-30 01:34:18 -0400

    updated loreal_spider script

--- loreal/loreal/spiders/loreal_spider.py
+++ loreal/loreal/spiders/loreal_spider.py
@@ -10,7 +10,33 @@ class LorealSpider(Spider):
 
 
 	def parse(self, repsonse):
-		num_pages_
+		num_pages_haircare = len(response.xpath('//div[@class="results-pagination"]/a').extract()) - 2
+
+		num_pages_skincare = len(response.xpath('//div[@class="results-pagination"]/a').extract()) - 2
+
+		num_pages_makeup = len(response.xpath('//div[@class="results-pagination"]/a').extract()) - 2
+
+		# Our focus is haircare but we want to extract other categories as well
+		haircare_urls = ['https://www.lorealparisusa.com/products/hair-care/shop-all-products.aspx?size=20&page={}'.format(x) for x in range(1, num_pages_haircare + 1)]
+
+		skincare_urls = ['https://www.lorealparisusa.com/products/skin-care/shop-all-products.aspx?size=20&page={}'.format(x) for x in range(1, num_pages_skincare + 1)]
+
+		makeup_urls = ['https://www.lorealparisusa.com/products/makeup/shop-all-products.aspx?size=20&page={}'.format(x) for x in range(1, num_pages_makeup + 1)]
+
+
+		result_urls = haircare_urls + skincare_urls + makeup_urls
+
+		for url in result_urls:
+			# For debugging
+            # print('='*55) 
+            # print(url)
+            # print('='*55)
+            # Just like request.get(url) which gives a response object
+			yield Request(url = url, callback = self.parse_result_page)
+
+
+
+
 
 
 
