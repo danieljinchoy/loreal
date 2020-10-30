a9eadfa9d7afc5a2d0208bb10d233159f06d040a
Daniel Choy <danieljinchoy@gmail.com>
2020-10-30 10:55:04 -0400

    update

--- loreal/loreal/settings.py
+++ loreal/loreal/settings.py
@@ -14,7 +14,7 @@ NEWSPIDER_MODULE = 'loreal.spiders'
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'loreal (+http://www.yourdomain.com)'
+USER_AGENT="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36"
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = False
--- loreal/loreal/spiders/loreal_spider.py
+++ loreal/loreal/spiders/loreal_spider.py
@@ -24,19 +24,76 @@ class LorealSpider(Spider):
 		makeup_urls = ['https://www.lorealparisusa.com/products/makeup/shop-all-products.aspx?size=20&page={}'.format(x) for x in range(1, num_pages_makeup + 1)]
 
 
-		result_urls = haircare_urls + skincare_urls + makeup_urls
+		urls = haircare_urls + skincare_urls + makeup_urls
 
-		for url in result_urls:
+		for url in urls:
 			# For debugging
             # print('='*55) 
             # print(url)
             # print('='*55)
-            # Just like request.get(url) which gives a response object
 			yield Request(url = url, callback = self.parse_result_page)
 
+	def parse_result_page(self, repsonse):
+		products = response.xpath('//div[@class="product-container"]')
+		
+		for product in products:
+
+            try:
+                url = 'https://www.lorealparisusa.com' + product.xpath('./a/@href').extract_first()
+            except:
+                continue
 
 
+            try:
+                product_name = product.xpath('./div[@class="product-container__data"]/div/a/h3/text()').extract_first()
+            except:
+                product_name = None
 
 
+            try:
+                product_price = product.xpath('./div[@class="product-container__actions"]/a/span/text()').extract_first()
+            except:
+                product_price = None
+
+            yield Request(url=url, callback=self.parse_detail_page, meta ={'name': product_name, 'price': product_price})
 
 
+    # Can I parse the review pages using scrapy?
+
+    def parse_detail_page(self, response):
+        name = response.meta['name']
+        price = response.meta['price']
+
+        # main category
+        try:
+            main_category = response.xpath('//span[@itemprop="name"]/text()').extract()[1]
+        except:
+            main_category = None
+
+        # category
+        try:
+            category = response.xpath('//span[@itemprop="name"]/text()').extract()[3]
+        except:
+            category = None
+
+        # rating
+        try:
+            rating = response.xpath('//span[@itemprop="ratingValue"]/text()').extract_first()
+        except:
+            rating = None
+
+        # number of reviews
+        try:
+            num_reviews = response.xpath('//span[@itemprop="reviewCount"]/text()').extract_first()
+        except:
+            num_reviews = None
+        
+        item = LorealItem()
+        item['name'] = name
+        item['price'] = price
+        item['rating'] = rating
+        item['category'] = category
+        item['num_reviews'] = num_reviews
+        item['main_category'] = main_category
+
+        yield item
